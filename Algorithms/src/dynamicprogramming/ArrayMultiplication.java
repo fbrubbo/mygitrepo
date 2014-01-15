484f39afdcd7214b239e2e909c837c37eb798972
Fernando <Fernando@dbs-note-32>
2014-01-14 22:23:31 -0200

    a

---
+++ Algorithms/src/dynamicprogramming/ArrayMultiplication.java
@@ -0,0 +1,48 @@
+package dynamicprogramming;
+
+/**
+ * There is an array A[N] of N numbers. 
+ * You have to compose an array Output[N] such that Output[i] will be equal to multiplication of 
+ * all the elements of A[N] except A[i]. For example Output[0] will be multiplication of A[1] to A[N-1] 
+ * and Output[1] will be multiplication of A[0] and from A[2] to A[N-1]. 
+ * 
+ * Solve it without division operator and in O(n).
+ */
+public class ArrayMultiplication {
+
+	public static int[] multiplyAllButCurrentElement(int[] a) {
+		if(a.length==0)
+			return a;
+		
+ 		int[] output = new int[a.length];
+		
+		output[0] = 1;
+		for(int i=1; i<a.length; i++) {
+			output[i] = a[i-1] * output[i-1];
+		}
+		
+		int total = 1;
+		for(int i=a.length-2; i>=0; i--) {
+			total *= a[i+1];
+			output[i] = total * output[i];
+		}
+		
+		return output;
+
+		
+/*		
+ 		int[] output = new int[a.length];
+		
+		int total = 1;
+		for(int i=0; i<a.length; i++) {
+			total *= a[i];
+		}
+		
+		for(int i=0; i<a.length; i++) {
+			output[i] = total/a[i];
+		}
+		
+		return output;
+*/
+	}
+}
---
+++ Algorithms/test/dynamicprogramming/ArrayMultiplicationTest.java
@@ -0,0 +1,49 @@
+package dynamicprogramming;
+
+import static dynamicprogramming.ArrayMultiplication.multiplyAllButCurrentElement;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+public class ArrayMultiplicationTest {
+
+	@Test
+	public void testEmptyArray(){
+		int[] a = new int[]{};
+		int[] out = multiplyAllButCurrentElement(a);
+		assertThat(out, is(equalTo(a)));
+	}
+
+	@Test
+	public void testArrayWithOnlyOneElement(){
+		int[] a = new int[]{4};
+		int[] out = multiplyAllButCurrentElement(a);
+		assertThat(out, is(equalTo(new int[]{1})));
+	}
+
+	@Test
+	public void testArrayWithtwoElements(){
+		int[] a = new int[]{5, 3};
+		int[] out = multiplyAllButCurrentElement(a);
+		assertThat(out, is(equalTo(new int[]{3, 5})));
+	}
+	
+	@Test
+	public void testArrayWithThreeElements(){
+		int[] a = new int[]{4, 2, 3};
+		int[] out = multiplyAllButCurrentElement(a);
+		assertThat(out, is(equalTo(new int[]{6, 12, 8})));
+	}
+
+	@Test
+	public void testArrayWithManyElement(){
+		int[] a = new int[]{4, 2, 3, 5};
+		int[] out = multiplyAllButCurrentElement(a);
+		assertThat(out, is(equalTo(new int[] { 30, 60, 40, 24 })));
+	}
+
+	
+}
